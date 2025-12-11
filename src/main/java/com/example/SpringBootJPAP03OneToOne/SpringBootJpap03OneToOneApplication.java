aa8fd2efa11de78f893831ba532f37fcf694fbc9
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-11 10:38:14 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringBootJPAP03OneToOne/SpringBootJpap03OneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJPAP03OneToOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpap03OneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpap03OneToOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootJPAP03OneToOne/SpringBootJpap03OneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJPAP03OneToOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpap03OneToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
